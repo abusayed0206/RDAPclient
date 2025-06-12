0632ec17b93d8da996024fb6c278d0e86250fba2
MD. ABU SAYED <hello@sayed.page>
2025-06-12 21:14:44 +0600

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
+/** @type {import("prettier").Config} */
 module.exports = {
   arrowParens: 'always',
   singleQuote: true,
   jsxSingleQuote: true,
   tabWidth: 2,
   semi: true,
+  plugins: ['prettier-plugin-tailwindcss'],
 };
